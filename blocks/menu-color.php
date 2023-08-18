1af674af88a6e68db018476479e51dc0b28fcb85
luisavilhena <luisavilhena@gmail.com>
2023-08-18 19:55:12 -0300

    retira nav_menu_link em menu-color.php

--- blocks/menu-color.php
+++ blocks/menu-color.php
@@ -9,13 +9,13 @@ Container::make( 'nav_menu_item', __( 'Menu Settings' ) )
     ));
 
 
-add_filter( 'nav_menu_link_attributes', 'crb_nav_menu_link_attributes', 10, 4 );
-function crb_nav_menu_link_attributes( $atts, $item, $args, $depth ) {
-    $crb_color = carbon_get_nav_menu_item_meta( $item->ID, 'crb_color' );
-    $atts['style'] = ! empty( $crb_color ) ? 'color: ' . $crb_color . '; ' : '';
+// add_filter( 'nav_menu_link_attributes', 'crb_nav_menu_link_attributes', 10, 4 );
+// function crb_nav_menu_link_attributes( $atts, $item, $args, $depth ) {
+//     $crb_color = carbon_get_nav_menu_item_meta( $item->ID, 'crb_color' );
+//     $atts['style'] = ! empty( $crb_color ) ? 'color: ' . $crb_color . '; ' : '';
 
-    return $atts;
-}
+//     return $atts;
+// }
 
 
-add_action( 'carbon_fields_register_fields', 'nav_menu_link_attributes' );
+// add_action( 'carbon_fields_register_fields', 'nav_menu_link_attributes' );
